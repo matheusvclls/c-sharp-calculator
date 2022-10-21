518b0259b3e50901d89d9ef6b9dcfa887ad0bc5a
Matheus Vasconcellos <matheus.vasconcellos@inallmedia.com>
2022-10-21 08:59:53 -0300

    feat: add mathematical operartors to the calculator

--- Program.cs
+++ Program.cs
@@ -7,11 +7,29 @@
             Console.WriteLine("Hi, let's learn C#!\n");
             Console.Write("Enter a number: ");
             double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("What mathematical operation do you want to calculate?: ");
+            string math_op =  Console.ReadLine();
+
             Console.Write("Enter another number: ");
             double number2 = Convert.ToDouble(Console.ReadLine());
-            
-            
-            Console.WriteLine("The sum of these numbers is: " + (number1+number2) +"\n");
+
+            if(math_op == "+")
+            {
+                Console.WriteLine(number1+number2);
+            } 
+            else if(math_op=="-") 
+            {
+                Console.WriteLine(number1-number2);
+            } 
+            else if(math_op=="*")
+            {
+                Console.WriteLine(number1-number2);
+            } 
+            else if(math_op=="/")
+            {
+                Console.WriteLine(number1/number2);
+            }
         }
     }
 }
